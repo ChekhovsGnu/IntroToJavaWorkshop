5f777b7bc081c6c966c2d43f571ddba80cd23885
Revanty <league@WTS14.attlocal.net>
2016-04-07 15:02:40 -0700

    fdhsk
    fads

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -3,27 +3,48 @@ package day3;
 import javax.swing.JOptionPane;
 
 /**
- * Teacher's Note: 
- * Have the kids play with the Speak & Spell. 
- * The first Speak & Spell was introduced at the summer Consumer Electronics Show in June 1978, making it 
- * one of the earliest handheld electronic devices with a visual display to use interchangeable game cartridges. 
- * Discuss with students how you would make this program. 
- * Allow them to code it themselves, or use this recipe.
+ * Teacher's Note: Have the kids play with the Speak & Spell. The first Speak & Spell was introduced at the summer
+ * Consumer Electronics Show in June 1978, making it one of the earliest handheld electronic devices with a visual
+ * display to use interchangeable game cartridges. Discuss with students how you would make this program. Allow them to
+ * code it themselves, or use this recipe.
  **/
 
 public class SpeakAndSpell {
-
 	public static void main(String[] args) {
-		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
-		// 2. Catch the user's answer in a String
-
-		// 3. If the user spelled the word correctly, speak "correct"
-
-		// 4. Otherwise say "wrong"
-
-		// 5. repeat the process for other words
-		
+		speak("spell league, as in the league of amazing programmers");
+		String answer = JOptionPane.showInputDialog("Spell the word");
+		if (answer.equalsIgnoreCase("league")) {
+			speak("Correct.");
+		} else {
+			speak("Wrong. League is spelled L E A G U E");
+		}
+		speak("spell talc, as in talc is one of the softest minerals");
+		answer = JOptionPane.showInputDialog("Spell the word");
+		if (answer.equalsIgnoreCase("talc")) {
+			speak("Correct.");
+		} else {
+			speak("Wrong. talc is spelled T A L C");
+		}
+		answer = JOptionPane.showInputDialog("Spell the word");
+		if (answer.equalsIgnoreCase("cryptic")) {
+			speak("Correct.");
+		} else {
+			speak("Wrong. Cryptic is spelled C R Y P T I C");
+		}
+		speak("spell quadriplegic, as in the accident made him a quadriplegic");
+		answer = JOptionPane.showInputDialog("Spell the word");
+		if (answer.equalsIgnoreCase("rhythm")) {
+			speak("Correct.");
+		} else {
+			speak("Wrong. Quadriplegic is spelled Q U A D R I P L E G I C");
+		}
+		speak("spell the quick brown fox jumped over the lazy dog, as in yesterday the quick brown fox jumped over the lazy dog when I wasn't looking");
+		answer = JOptionPane.showInputDialog("Spell the words");
+		if (answer.equalsIgnoreCase("The quick brown fox jumped over the lazy dog")) {
+			speak("Correct.");
+		} else {
+			speak("Wrong. The quick brown fox jumped over the lazy dog is spelled T H E space Q U I C K space B R O W N space F O X space J U M P E D space O V E R space T H E space L A Z Y space D O G");
+		}
 	}
 
 	static void speak(String words) {
@@ -35,5 +56,3 @@ public class SpeakAndSpell {
 	}
 
 }
-
-
